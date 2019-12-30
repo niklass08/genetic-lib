77163c6accf0bdc6f6413e456633fc2572c2dd14
Nicolas Babel <n.babel@lectra.com>
2019-12-30 08:47:31 +0100

    chore: config editor eslint prettier

--- geneticOptimizer.js
+++ geneticOptimizer.js
@@ -1,31 +1,29 @@
-export class geneticOptimizer{
+export class geneticOptimizer {
+  constructor({ initialStateGenerator, mutator, fitnessEvaluator }) {
+    //Function that returns an array of random states
+    this.initialStateGenerator = initialStateGenerator;
 
-    constructor({initialStateGenerator, mutator, fitnessEvaluator}){
-        //Function that returns an array of random states
-        this.initialStateGenerator = initialStateGenerator;
+    //Function that slightly modify a state
+    this.mutator = mutator;
 
-        //Function that slightly modify a state
-        this.mutator = mutator;
+    //Function that associate a fitness score to a given state
+    this.fitnessEvaluator = fitnessEvaluator;
+  }
 
-        //Function that associate a fitness score to a given state
-        this.fitnessEvaluator = fitnessEvaluator;
+  *optimize() {
+    //Generate a population that is an array of states
+    const population = this.initialStateGenerator();
+
+    //Find Best state in the population
+    let bestFitness = Number.NEGATIVE_INFINITY;
+    const bestState = population.reduce((acc, curr) => {
+      let currentFitness = this.fitnessEvaluator(curr);
+      acc = currentFitness > bestFitness ? curr : acc;
+      bestFitness = currentFitness > bestFitness ? currentFitness : bestFitness;
+    });
+
+    while (true) {
+      yield population;
     }
-
-    * optimize(){
-        //Generate a population that is an array of states
-        const population = this.initialStateGenerator();
-
-        //Find Best state in the population
-        let bestFitness = Number.NEGATIVE_INFINITY;
-        const bestState = population.reduce((acc, curr) => {
-            let currentFitness = this.fitnessEvaluator(curr);
-            acc = currentFitness > bestFitness ? curr : acc;
-            bestFitness = currentFitness > bestFitness ? currentFitness : bestFitness;
-        });
-
-        
-        while(true){
-            yield population;
-        }
-    }
+  }
 }
